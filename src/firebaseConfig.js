06e2ad5b719eaeece3fba1523bf216da024c87ba
Lee Ryan <2solimanleeryan@gmail.com>
2022-12-17 20:18:19 +0800

    Add Firebase config file

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDFwjVYBrmTYYZHhiWyLarWW0DYoGV7Qwc",
+    authDomain: "chat-app-2075e.firebaseapp.com",
+    projectId: "chat-app-2075e",
+    storageBucket: "chat-app-2075e.appspot.com",
+    messagingSenderId: "327700515397",
+    appId: "1:327700515397:web:2923da1fa324cb42e6712c"
+};
+
+export default firebaseConfig;
