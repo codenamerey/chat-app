386fc5798d81a0ad7eaf3b5fc9c8d9308493a84a
Lee Ryan <2solimanleeryan@gmail.com>
2022-12-18 17:16:10 +0800

    Display messages

--- src/index.js
+++ src/index.js
@@ -26,6 +26,9 @@ initializeApp(firebaseConfig);
 
 const signLogButton = document.querySelector('#login-signout');
 const loggedInUser = document.querySelector('#logged-in-user');
+const messageInput = document.querySelector('#message-input');
+const sendButton = document.querySelector('#send-button');
+let messages = document.querySelector('.messages');
 
 const handleSignLogClick = async() => {
     if(getAuth().currentUser != null) {
@@ -61,5 +64,55 @@ const authStateObserver = () => {
         loggedInUser.textContent = '';
     }
 }
+
+const sendMessage = async() => {
+    await addDoc(collection(getFirestore(), 'messages'), {
+        name: getAuth().currentUser.displayName,
+        profile: getAuth().currentUser.photoURL,
+        message: messageInput.value,
+        timeStamp: serverTimestamp(),
+        uid: getAuth().currentUser.uid
+    });
+    messageInput.value = '';
+    messageInput.focus();
+}
+
+const readMessage = () => {
+    const messagesQuery = query(collection(getFirestore(), 'messages'), orderBy('timeStamp'));
+
+    onSnapshot(messagesQuery, (snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+            let message = change.doc.data();
+            displayMessage(message.uid, message.timeStamp, message.message, message.profile, message.name)
+        });
+    });
+}
+
+const displayMessage = (uid, timeStamp, message, profile, name) => {
+    const messageDiv = document.createElement('div');
+    const profileImg = document.createElement('img');
+    const messageParagraph = document.createElement('p');
+    const h3 = document.createElement('h3');
+    const time = document.createElement('p');
+
+    const textDetailsDiv = document.createElement('div');
+    profileImg.src = profile;
+    messageParagraph.textContent = message;
+    h3.textContent = name;
+    time.textContent = (timeStamp.toDate()).toString().substring(4, 15);
+    textDetailsDiv.append(h3, messageParagraph, time);
+    messageDiv.append(profileImg, textDetailsDiv);
+    //design messagediv with Tailwindcss
+    textDetailsDiv.className = 'bg-sky-900 p-3 rounded';
+    messageDiv.className = 'flex w-4/5 p-6 space-x-2';
+    profileImg.className = 'rounded-full w-14 h-14';
+    messageParagraph.className = 'text-white';
+    // h3.className = 'font-bold';
+    messages.appendChild(messageDiv);
+}
+
 initFirebaseAuth();
+readMessage();
+
 signLogButton.addEventListener('click', handleSignLogClick);
+sendButton.addEventListener('click', sendMessage);
